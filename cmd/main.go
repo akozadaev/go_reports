ae4e4023df41754056b054a2bc53f071aa4ec5df
Alexey Kozadaev <akozadaev@skillcup.ru>
2024-02-17 09:54:35 +0300

    add command

---
+++ cmd/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/spf13/cobra"
+	"log"
+)
+
+func init() {
+	envPath := "."
+	envFileName := ".env"
+
+	fullPath := envPath + "/" + envFileName
+
+	if err := godotenv.Overload(fullPath); err != nil {
+		log.Printf("[ERROR] failed with %+v", "No .env file found")
+	}
+}
+
+func main() {
+	commandName := "run:root:cmd"
+	rootCmd := &cobra.Command{
+		Use: commandName,
+	}
+
+	subCmd := &cobra.Command{
+		Use:   "use",
+		Short: "Use sub cmd",
+		Long:  "Use sub cmd",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Executed sub cmd")
+		},
+	}
+	rootCmd.AddCommand(subCmd)
+	rootCmd.Execute()
+}
